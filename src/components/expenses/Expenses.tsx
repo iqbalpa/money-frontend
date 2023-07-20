c737b9bdc41e71dac2ce3224d9c49797c90868a2
iqbalpa <iqbalpahlevi16@gmail.com>
2023-07-20 10:41:46 +0700

    style: update table expenses

--- src/components/expenses/Expenses.tsx
+++ src/components/expenses/Expenses.tsx
@@ -31,30 +31,78 @@ export default function Expenses() {
 
   return (
     <>
-      <table className='table-auto'>
-        <thead>
-          <tr>
-            <th className='border px-4 py-2'>Date</th>
-            <th className='border px-4 py-2'>Amount</th>
-            <th className='border px-4 py-2'>Category</th>
-            <th className='border px-4 py-2'>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {dummyExpenses.map((expense) => (
-            <tr key={expense.id}>
-              <td className='border px-4 py-2'>{expense.date.split('T')[0]}</td>
-              <td className='border px-4 py-2'>{expense.amount}</td>
-              <td className='border px-4 py-2'>{expense.category}</td>
-              <td className='border px-4 py-2'>
-                <button className='rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700'>
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className='flex flex-col'>
+        <div className='overflow-x-auto'>
+          <div className='inline-block w-full p-1.5 align-middle'>
+            <div className='overflow-hidden rounded-lg border'>
+              <table className='min-w-full divide-y divide-gray-200'>
+                <thead className='bg-gray-50'>
+                  <tr>
+                    <th
+                      scope='col'
+                      className='px-6 py-3 text-left text-xs font-bold uppercase text-gray-500 '
+                    >
+                      Date
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-6 py-3 text-left text-xs font-bold uppercase text-gray-500 '
+                    >
+                      Amount
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-6 py-3 text-left text-xs font-bold uppercase text-gray-500 '
+                    >
+                      Category
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-6 py-3 text-right text-xs font-bold uppercase text-gray-500 '
+                    >
+                      Edit
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-6 py-3 text-right text-xs font-bold uppercase text-gray-500 '
+                    >
+                      Delete
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className='divide-y divide-gray-200'>
+                  {dummyExpenses.map((expense) => (
+                    <tr key={expense.id}>
+                      <td className='whitespace-nowrap px-6 py-4 text-sm font-medium text-gray-800'>
+                        {expense.date.split('T')[0]}
+                      </td>
+                      <td className='whitespace-nowrap px-6 py-4 text-sm text-gray-800'>
+                        {expense.amount}
+                      </td>
+                      <td className='whitespace-nowrap px-6 py-4 text-sm text-gray-800'>
+                        {expense.category}
+                      </td>
+                      <td className='whitespace-nowrap px-6 py-4 text-right text-sm font-medium'>
+                        <a
+                          className='text-green-500 hover:text-green-700'
+                          href='#'
+                        >
+                          Edit
+                        </a>
+                      </td>
+                      <td className='whitespace-nowrap px-6 py-4 text-right text-sm font-medium'>
+                        <a className='text-red-500 hover:text-red-700' href='#'>
+                          Delete
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
